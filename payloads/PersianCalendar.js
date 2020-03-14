3c373ae14c1b25363acd01b2f18edfb71c103c7c
Ps4lite <62132865+Ps4lite@users.noreply.github.com>
2020-03-14 17:11:03 +0330

    BlueBox v2.5.1 by REZA

--- payloads/PersianCalendar.js
+++ payloads/PersianCalendar.js
@@ -1,167 +1,83 @@
-// JavaScript Document
 
-
-//get name of Day.
-		function DayName(DayNumber){
-		var dayname;
-	
-		switch(DayNumber){
-			case 6:
-			dayname="شنبه";
-			break;
-		
-				case 0:
-				dayname="یکشنبه";
-				break;
-		
-					case 1:
-					dayname="دوشنبه";
-					break;
-		
-						case 2:
-						dayname="سه شنبه";
-						break;
-		
-							case 3:
-							dayname="چهارشنبه";
-							break;
-		
-								case 4:
-								dayname="پنجشنبه";
-								break;
-		
-									case 5:
-									dayname="جمعه";
-									break;
-		
-	}
-
-
-	return dayname;
-}
-
-	
-		//get name of month.
-		function MonthName(MonthNumber){
-			var monthname;
-	
-			switch (MonthNumber){
-		
-			case 0:
-			monthname="فروردین";
-			break;
-	
-				case 1:
-				monthname="اردیبهشت";
-				break;
-	
-					case 2:
-					monthname="خرداد";
-					break;
-	
-						case 3:
-						monthname="تیر";
-						break;
-	
-							case 4:
-							monthname="مرداد";
-							break;
-	
-								case 5:
-								monthname="شهریور";
-								break;
-	
-									case 6:
-									monthname="مهر";
-									break;
-	
-										case 7:
-										monthname="آبان";
-										break;
-	
-											case 8:
-											monthname="آذر";
-											break;
-	
-												case 9:
-												monthname="دی";
-												break;
-	
-													case 10:
-													monthname="بهمن";
-													break;
-	
-														case 11:
-														monthname="اسفند";
-														break;
-	
-		
-	}
-	
-	return monthname;
-}
-//persian calendar
-function PersianCalendar(DateType,CalendarTitle){
-	
-	var PC =new Object;
-	
-	//Base Dates ()
-	var basedate=new Date(2015,7,5);
-	var pbasedate=new Date(1394,4,14);
-
-	// Diffrence of Base Dates
-	var diff=basedate.getTime() - pbasedate.getTime() ;
-
-	//Get Today Date
-	var today=new Date();
-	today=Date.now();
-
-	//Calculate Persian Date.
-	var persianDate= new Date(today-diff);
-
-
-		
-
-
-//Create Persian Date string.
-//getMonth--> Returns 0-11.
-//getFullYear--> Returns year(YYYY).
-//getDate--> Returns 1-31 Day of month.
-//getDay-->0-6 day of week, 0==Sunday.
-//getHours-->0-23.
-//getTime-->milliseconds since 1/1/1970.
-
-
-	var pcstring;
-
-	switch (DateType){
-	case 0:
-		pcstring=String(String(persianDate.getFullYear())+ "/" + String(persianDate.getMonth()+1) +"/"+ 
-		 String(persianDate.getDate()+1));
-	
-		break;
-	
-	case 1:
-		pcstring=String( DayName(persianDate.getDay())+ "  " + String(persianDate.getDate()-1) + "  " + 
-		 MonthName(persianDate.getMonth()) + "  " +String(persianDate.getFullYear()) );
-				
-		break;
-	
-}
-
-//set object properties
-PC.name=CalendarTitle;
-PC.year=persianDate.getFullYear();
-PC.month=persianDate.getMonth()+1;
-PC.monthname=MonthName(persianDate.getMonth());
-PC.day=persianDate.getDate()+1;
-PC.dayname=String(DayName(persianDate.getDay()));
-PC.type=DateType;
-PC.date= pcstring;
-
-
-
-return PC;
-
-}
-
+//persian date and time
+function showdate() {
+ week= new Array("يكشنبه","دوشنبه","سه شنبه","چهارشنبه","پنج شنبه","جمعه","شنبه");
+ months = new Array("فروردين","ارديبهشت","خرداد","تير","مرداد","شهريور","مهر","آبان","آذر","دی","بهمن","اسفند");
+ a = new Date();
+ d= a.getDay();
+ Hour= a.getHours();
+ Minute= a.getMinutes();
+ Second= a.getSeconds();
+ if(Minute<10){
+ Minute="0"+Minute;
+ }
+ if(Second<10){
+ Second="0"+Second;
+ }
+ if(Hour<10){
+ Hour="0"+Hour;
+ }
+ day= a.getDate();
+ month = a.getMonth()+1;
+ year= a.getYear()-100;
+ year = (year== 0)?2000:year;
+ (year<1000)? (year += 2000):true;
+ year -= ( (month < 3) || ((month == 3) && (day < 21)) )? 622:621;
+ switch (month) {
+ case 1: (day<21)? (month=10, day+=10):(month=11, day-=20); break;
+ case 2: (day<20)? (month=11, day+=11):(month=12, day-=19); break;
+ case 3: (day<21)? (month=12, day+=9):(month=1, day-=20);
+ break;
+ case 4: (day<21)? (month=1, day+=11):(month=2, day-=20);
+ break;
+ case 5:
+ case 6: (day<22)? (month-=3, day+=10):(month-=2, day-=21); break;
+ case 7:
+ case 8:
+ case 9: (day<23)? (month-=3, day+=9):(month-=2, day-=22);
+ break;
+ case 10:(day<23)? (month=7, day+=8):(month=8, day-=22);
+ break;
+ case 11:
+ case 12:(day<22)? (month-=3, day+=9):(month-=2, day-=21);
+ break;
+ default:
+ break;
+ }
+ 
+ function gregorian_to_jalali(gy,gm,gd){
+ var g_d_m,jy,jm,jd,gy2,days;
+ g_d_m=[0,31,59,90,120,151,181,212,243,273,304,334];
+ if(gy > 1600){
+ jy=979;
+ gy-=1600;
+ }else{
+ jy=0;
+ gy-=621;
+ }
+ gy2=(gm > 2)?(gy+1):gy;
+ days=(365*gy) +(parseInt((gy2+3)/4)) -(parseInt((gy2+99)/100)) +(parseInt((gy2+399)/400)) -80 +gd +g_d_m[gm-1];
+ jy+=33*(parseInt(days/12053)); 
+ days%=12053;
+ jy+=4*(parseInt(days/1461));
+ days%=1461;
+ if(days > 365){
+ jy+=parseInt((days-1)/365);
+ days=(days-1)%365;
+ }
+ jm=(days < 186)?1+parseInt(days/31):7+parseInt((days-186)/30);
+ jd=1+((days < 186)?(days%31):((days-186)%30));
+ return [jy,jm,jd];
+ }
+ 
+ ndt=new Date();
+ g_y=ndt.getFullYear();
+ g_m=ndt.getMonth()+1;
+ g_d=ndt.getDate();
+ shamsi=gregorian_to_jalali(g_y,g_m,g_d);
+ var nday = shamsi[2];
+ 
+ $("#timec").text(week[d]+" "+nday+" "+months[month-1]+" "+ year);
+ $("#clock").text(Hour+":"+Minute+":"+Second);
+ }
+ setInterval(showdate,10);
